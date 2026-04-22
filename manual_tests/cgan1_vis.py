e81269f8be7c055786edbb9b2f160c7018ef0013
Pawel Trajdos <pawel.trajdos@pwr.edu.pl>
2026-04-22 21:02:18 +0200

    1. cgan1_vis -- saving to pdf, write mmd to file.

--- manual_tests/cgan1_vis.py
+++ manual_tests/cgan1_vis.py
@@ -10,6 +10,7 @@ from raw_emg_aug_tsgm.data_augmentation.model_factories.cgan_factory import CGAN
 from raw_emg_aug_tsgm.data_augmentation.tsgm_nn_augmenter_wrapper import (
     TSGMANNAugmenterWrapper,
 )
+from tsgm.metrics.metrics import MMDMetric
 
 from tsgm.models.architectures import (
     cGAN_LSTMConv3Architecture,
@@ -18,37 +19,52 @@ from tsgm.models.architectures import (
 )
 from tensorflow.keras import mixed_precision
 import tensorflow as tf
+from matplotlib.backends.backend_pdf import PdfPages
 # tf.keras.backend.set_floatx('float16')
 
 # mixed_precision.set_global_policy('mixed_float16')
 
-N, R, C = 1000, 50, 1
+N, R, C = 500, 50, 1
 
 set_creat = RawSignalsCreatorSines(
     set_size=N, column_number=C, samples_number=R, dtype=np.float16
 )
 raw_data: RawSignals = set_creat.get_set()
+rep_file_path = "vis.md"
 
-rd = raw_data[0].to_numpy()
-plt.plot(rd)
-plt.show()
+with PdfPages('cgan1_vis.pdf') as pdf, open(rep_file_path, "w") as rep_file:
+    rd = raw_data[0].to_numpy()
+    plt.plot(rd)
+    plt.title("Original signal")
+    pdf.savefig()
+    plt.close()
 
-opts = {
-    "d_optimizer": keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5),
-    "g_optimizer": keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5),
-    "loss_fn": keras.losses.BinaryCrossentropy(),
-}
+    opts = {
+        "d_optimizer": keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5),
+        "g_optimizer": keras.optimizers.legacy.Adam(learning_rate=0.0002, beta_1=0.5),
+        "loss_fn": keras.losses.BinaryCrossentropy(),
+    }
 
-gen = TSGMANNAugmenterWrapper(
-    model_factory=CGANFactory(model_compile_options=opts),
-    normalize_channels=True,
-    forced_keras_dtype=np.float16,
-)
-gen.fit(raw_data, epochs=20, batch_size=32)
+    gen = TSGMANNAugmenterWrapper(
+        model_factory=CGANFactory(model_compile_options=opts),
+        normalize_channels=True,
+        forced_keras_dtype=np.float16,
+    )
+    gen.fit(raw_data, epochs=10, batch_size=32)
 
-generted = gen.transform(raw_data)
+    generted = gen.transform(raw_data)
 
-r = generted[0].to_numpy()
+    r = generted[0].to_numpy()
 
-plt.plot(r)
-plt.show()
+    mmd_metric = MMDMetric()
+    mmd_value = mmd_metric(raw_data.to_numpy(), generted.to_numpy())
+    rep_file.write(f"MMD between original and generated data: {mmd_value}\n")
+
+
+
+    plt.plot(r)
+    plt.title("Generated signal")
+    pdf.savefig()
+    plt.close()
+    
+
